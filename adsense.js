03719e9845e59050d64a4fb62f0128da2624f1fe
Brian Hyder <brianhyder@gmail.com>
2015-03-26 21:24:27 -0400

    Converted to 0.4.0

--- adsense.js
+++ adsense.js
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2014  PencilBlue, LLC
+    Copyright (C) 2015  PencilBlue, LLC
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,126 +15,148 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/**
- * Adsense - Display Adsense ads on your site.
- *
- * @author Blake Callens <blake@pencilblue.org>
- * @copyright 2014 PencilBlue, LLC
- */
-function Adsense(){}
+module.exports = function(pb) {
+    
+    //pb dependencies
+    var util                = pb.util;
+    var DAO                 = pb.DAO;
+    var CustomObjectService = pb.CustomObjectService;
+    var TemplateService     = pb.TemplateService;
+    var PluginService       = pb.PluginService;
 
-/**
- * Called when the application is being installed for the first time.
- *
- * @param cb A callback that must be called upon completion.  cb(err, result).
- * The result is ignored
- */
-Adsense.onInstall = function(cb) {
-    var self = this;
-    var cos = new pb.CustomObjectService();
+    /**
+     * Adsense - Display Adsense ads on your site.
+     *
+     * @author Blake Callens <blake@pencilblue.org>
+     * @class Adsense
+     * @constructor
+     */
+    function Adsense(){}
 
-    cos.loadTypeByName('adsense_ad', function(err, adsenseAd) {
-        if(!adsenseAd) {
-            var adValues = {name: 'adsense_ad', fields: {name: {field_type: 'text'}, ad_id: {field_type: 'number'}}};
-            cos.saveType(adValues, function(err, adsenseAd) {
-                if(err) {
-                    // TODO error handling
+    /**
+     * Called when the application is being installed for the first time.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    Adsense.onInstall = function(cb) {
+        var self = this;
+        
+        var cos = new CustomObjectService();
+        cos.loadTypeByName('adsense_ad', function(err, adsenseAd) {
+            if (util.isError(err)) {
+                return cb(err);
+            }
+            
+            //don't bother if it is already created
+            if (adsenseAd) {
+                return cb(null, true);
+            }
+            
+            var adValues = {
+                name: 'adsense_ad', 
+                fields: {
+                    name: {
+                        field_type: 'text'
+                    }, 
+                    ad_id: {
+                        field_type: 'number'
+                    }
                 }
-
-                cb(null, true);
-            });
-        }
-        else {
-            cb(null, true);
-        }
-    });
-};
-
-/**
- * Called when the application is uninstalling this plugin.  The plugin should
- * make every effort to clean up any plugin-specific DB items or any in function
- * overrides it makes.
- *
- * @param cb A callback that must be called upon completion.  cb(err, result).
- * The result is ignored
- */
-Adsense.onUninstall = function(cb) {
-    cb(null, true);
-};
-
-/**
- * Called when the application is starting up. The function is also called at
- * the end of a successful install. It is guaranteed that all core PB services
- * will be available including access to the core DB.
- *
- * @param cb A callback that must be called upon completion.  cb(err, result).
- * The result is ignored
- */
-Adsense.onStartup = function(cb) {
-    var self = this;
-    var cos = new pb.CustomObjectService();
-    var dao = new pb.DAO();
-
-    this.setGlobal = function(adsenseAd) {
-        pb.TemplateService.registerGlobal('adsense_' + adsenseAd.name, function(flag, cb) {
-            pb.plugins.getSetting('ad_client_id', 'adsense-pencilblue', function(err, adClientId) {
-                var adName = flag.split('adsense_').join('');
-                cos.loadTypeByName('adsense_ad', function(err, adsenseAdType) {
-                    cos.findByType(adsenseAdType, {name: adName}, function(err, adsenseAds) {
-                        if(!adsenseAds.length) {
-                            cb(err, '');
-                            return;
-                        }
-
-                        var adsenseAd = adsenseAds[0];
-                        cb(null, new pb.TemplateValue('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-client="' + adClientId + '" data-ad-slot="' + adsenseAd.ad_id + '" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>', false));
-                    });
-                });
+            };
+            cos.saveType(adValues, function(err, adsenseAd) {
+                cb(err, true);
             });
         });
     };
 
-    cos.loadTypeByName('adsense_ad', function(err, adsenseAdType) {
-        cos.findByType(adsenseAdType, null, function(err, adsenseAds) {
-            for(var i = 0; i < adsenseAds.length; i++) {
-                self.setGlobal(adsenseAds[i]);
-            }
+    /**
+     * Called when the application is uninstalling this plugin.  The plugin should
+     * make every effort to clean up any plugin-specific DB items or any in function
+     * overrides it makes.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    Adsense.onUninstall = function(cb) {
+        cb(null, true);
+    };
+
+    /**
+     * Called when the application is starting up. The function is also called at
+     * the end of a successful install. It is guaranteed that all core PB services
+     * will be available including access to the core DB.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    Adsense.onStartup = function(cb) {
+        var self = this;
+        var cos = new CustomObjectService();
+        var dao = new DAO();
+
+        this.setGlobal = function(adsenseAd) {
+            TemplateService.registerGlobal('adsense_' + adsenseAd.name, function(flag, cb) {
+                
+                var pluginService = new PluginService();
+                pluginService.getSetting('ad_client_id', 'adsense-pencilblue', function(err, adClientId) {
+                    var adName = flag.split('adsense_').join('');
+                    cos.loadTypeByName('adsense_ad', function(err, adsenseAdType) {
+                        cos.findByType(adsenseAdType, {name: adName}, function(err, adsenseAds) {
+                            if(!adsenseAds.length) {
+                                return cb(err, '');
+                            }
+
+                            var adsenseAd = adsenseAds[0];
+                            cb(err, new pb.TemplateValue('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-client="' + adClientId + '" data-ad-slot="' + adsenseAd.ad_id + '" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>', false));
+                        });
+                    });
+                });
+            });
+        };
+
+        cos.loadTypeByName('adsense_ad', function(err, adsenseAdType) {
+            cos.findByType(adsenseAdType, null, function(err, adsenseAds) {
+                for(var i = 0; i < adsenseAds.length; i++) {
+                    self.setGlobal(adsenseAds[i]);
+                }
+            });
+
+            pb.AdminSubnavService.registerFor('plugin_settings', function(navKey, localization, plugin) {
+                if(plugin.uid === 'adsense-pencilblue') {
+                    return [
+                        {
+                            name: 'ads',
+                            title: 'Manage Ads',
+                            icon: 'bookmark',
+                            href: '/admin/content/custom_objects/manage_objects/' + adsenseAdType[DAO.getIdField()].toString()
+                        },
+                        {
+                            name: 'refresh',
+                            title: 'Refresh Directives',
+                            icon: 'refresh',
+                            href: '/admin/plugins/settings/adsense-pencilblue/refresh'
+                        }
+                    ];
+                }
+                return [];
+            });
         });
 
-        pb.AdminSubnavService.registerFor('plugin_settings', function(navKey, localization, plugin) {
-            if(plugin.uid === 'adsense-pencilblue') {
-                return [
-                    {
-                        name: 'ads',
-                        title: 'Manage Ads',
-                        icon: 'bookmark',
-                        href: '/admin/content/custom_objects/manage_objects/' + adsenseAdType._id.toString()
-                    },
-                    {
-                        name: 'refresh',
-                        title: 'Refresh Directives',
-                        icon: 'refresh',
-                        href: '/admin/plugins/settings/adsense-pencilblue/refresh'
-                    }
-                ];
-            }
-            return [];
-        });
-    });
+        cb(null, true);
+    };
 
-    cb(null, true);
+    /**
+     * Called when the application is gracefully shutting down.  No guarantees are
+     * provided for how much time will be provided the plugin to shut down.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    Adsense.onShutdown = function(cb) {
+        cb(null, true);
+    };
+
+    //exports
+    return Adsense;
 };
-
-/**
- * Called when the application is gracefully shutting down.  No guarantees are
- * provided for how much time will be provided the plugin to shut down.
- *
- * @param cb A callback that must be called upon completion.  cb(err, result).
- * The result is ignored
- */
-Adsense.onShutdown = function(cb) {
-    cb(null, true);
-};
-
-//exports
-module.exports = Adsense;
--- controllers/refresh.js
+++ controllers/refresh.js
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2014  PencilBlue, LLC
+    Copyright (C) 2015  PencilBlue, LLC
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,83 +15,98 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-function AdsenseRefresh(){}
+module.exports = function(pb) {
+    
+    //pb dependencies
+    var util            = pb.util;
+    var BaseController  = pb.BaseController;
+    var PluginService   = pb.PluginService;
+    var SecurityService = pb.SecurityService;
 
-//inheritance
-util.inherits(AdsenseRefresh, pb.BaseController);
+    /**
+     *
+     * @class AdsenseRefresh
+     * @constructor
+     */
+    function AdsenseRefresh(){}
+    util.inherits(AdsenseRefresh, BaseController);
 
-/**
- * This is the function that will be called by the system's RequestHandler.  It
- * will map the incoming route to the ones below and then instantiate this
- * prototype.  The request handler will then proceed to call this function.
- * Its callback should contain everything needed in order to provide a response.
- *
- * @method render
- * @see BaseController#render
- * @param cb The callback.  It does not require a an error parameter.  All
- * errors should be handled by the controller and format the appropriate
- *  response.  The system will attempt to catch any catastrophic errors but
- *  makes no guarantees.
- */
-AdsenseRefresh.prototype.render = function(cb) {
-    var self = this;
-    var cos = new pb.CustomObjectService();
+    /**
+     * This is the function that will be called by the system's RequestHandler.  It
+     * will map the incoming route to the ones below and then instantiate this
+     * prototype.  The request handler will then proceed to call this function.
+     * Its callback should contain everything needed in order to provide a response.
+     *
+     * @method render
+     * @see BaseController#render
+     * @param cb The callback.  It does not require a an error parameter.  All
+     * errors should be handled by the controller and format the appropriate
+     *  response.  The system will attempt to catch any catastrophic errors but
+     *  makes no guarantees.
+     */
+    AdsenseRefresh.prototype.render = function(cb) {
+        var self = this;
+        var cos = new pb.CustomObjectService();
 
-    cos.loadTypeByName('adsense_ad', function(err, adsenseAdType) {
-        cos.findByType(adsenseAdType, null, function(err, adsenseAds) {
-            for(var i = 0; i < adsenseAds.length; i++) {
-                self.setGlobal(adsenseAds[i]);
-            }
+        cos.loadTypeByName('adsense_ad', function(err, adsenseAdType) {
+            cos.findByType(adsenseAdType, null, function(err, adsenseAds) {
+                for(var i = 0; i < adsenseAds.length; i++) {
+                    self.setGlobal(adsenseAds[i]);
+                }
 
-            self.session.success = self.ls.get('ADSENSE_REFRESHED');
-            self.redirect('/admin/plugins/settings/adsense-pencilblue', cb);
+                self.session.success = self.ls.get('ADSENSE_REFRESHED');
+                self.redirect('/admin/plugins/settings/adsense-pencilblue', cb);
+            });
         });
-    });
-};
+    };
 
-AdsenseRefresh.prototype.setGlobal = function(adsenseAd) {
-    pb.TemplateService.registerGlobal('adsense_' + adsenseAd.name, function(flag, cb) {
-        pb.plugins.getSetting('ad_client_id', 'adsense', function(err, adClientId) {
-            var adName = flag.split('adsense_').join('');
-            cos.loadTypeByName('adsense_ad', function(err, adsenseAdType) {
-                cos.findByType(adsenseAdType, {name: adName}, function(err, adsenseAds) {
-                    if(!adsenseAds.length) {
-                        cb(err, '');
-                        return;
-                    }
+    //TODO remove this duplicate code
+    //TODO speed this up so it doesn't make multiple DB calls on EVERY request
+    AdsenseRefresh.prototype.setGlobal = function(adsenseAd) {
+        pb.TemplateService.registerGlobal('adsense_' + adsenseAd.name, function(flag, cb) {
+            
+            var pluginService = new PluginService();
+            pluginService.getSetting('ad_client_id', 'adsense', function(err, adClientId) {
+                var adName = flag.split('adsense_').join('');
+                cos.loadTypeByName('adsense_ad', function(err, adsenseAdType) {
+                    cos.findByType(adsenseAdType, {name: adName}, function(err, adsenseAds) {
+                        if(!adsenseAds.length) {
+                            return cb(err, '');
+                        }
 
-                    var adsenseAd = adsenseAds[0];
-                    cb(null, new pb.TemplateValue('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-client="' + adClientId + '" data-ad-slot="' + adsenseAd.ad_id + '" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>', false));
+                        var adsenseAd = adsenseAds[0];
+                        cb(err, new pb.TemplateValue('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block" data-ad-client="' + adClientId + '" data-ad-slot="' + adsenseAd.ad_id + '" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>', false));
+                    });
                 });
             });
         });
-    });
-};
+    };
 
-/**
- * Provides the routes that are to be handled by an instance of this prototype.
- * The route provides a definition of path, permissions, authentication, and
- * expected content type.
- * Method is optional
- * Path is required
- * Permissions are optional
- * Access levels are optional
- * Content type is optional
- *
- * @param cb A callback of the form: cb(error, array of objects)
- */
-AdsenseRefresh.getRoutes = function(cb) {
-    var routes = [
-        {
-            method: 'get',
-            path: "/admin/plugins/settings/adsense-pencilblue/refresh",
-            auth_required: true,
-            access_level: ACCESS_EDITOR,
-            content_type: 'text/html'
-        }
-    ];
-    cb(null, routes);
-};
+    /**
+     * Provides the routes that are to be handled by an instance of this prototype.
+     * The route provides a definition of path, permissions, authentication, and
+     * expected content type.
+     * Method is optional
+     * Path is required
+     * Permissions are optional
+     * Access levels are optional
+     * Content type is optional
+     *
+     * @param cb A callback of the form: cb(error, array of objects)
+     */
+    AdsenseRefresh.getRoutes = function(cb) {
+        var routes = [
+            {
+                method: 'get',
+                path: "/admin/plugins/settings/adsense-pencilblue/refresh",
+                auth_required: true,
+                access_level: SecurityService.ACCESS_EDITOR,
+                content_type: 'text/html'
+            }
+        ];
+        cb(null, routes);
+    };
 
-//exports
-module.exports = AdsenseRefresh;
+    //exports
+    return AdsenseRefresh;
+};
